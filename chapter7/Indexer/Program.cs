91a9f5135cb1c374df358a708eb4ae4e3af939f7
Stephen Hodgson <stephen@srhodgson.co.uk>
2022-07-06 22:17:03 +0100

    Finish Indexer
    Finish the Indexer program

--- chapter7/Indexer/Program.cs
+++ chapter7/Indexer/Program.cs
@@ -7,6 +7,29 @@ using System.Runtime.CompilerServices;
 
 namespace Indexer
 {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create an array with enough room.
+            KeyedArray ma = new KeyedArray(100);
+
+            // Save the ages of the Simpson kids 
+            ma["Bart"] = 8;
+            ma["Lisa"] = 10;
+            ma["Maggie"] = 2;
+
+            // Look up the age of Lisa
+            Console.WriteLine("Lets find Lisa's age");
+            int age = (int)ma["Lisa"];
+            Console.WriteLine("Lisa is {0}", age);
+
+            // Wait for the user to acknowledge the results 
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+
     public class KeyedArray
     {
         // The following string provides the "key" into the array --
@@ -21,6 +44,64 @@ namespace Indexer
         {
             _keys = new string[size];
             _arrayElements = new object[size];
+        }
+
+        // Find -- Find the index of the element corresponding to the 
+        // string targetKey (return a negative if it can't be found).
+        private int Find(string targetKey)
+        {
+            for (int i = 0; i < _keys.Length; i++)
+            {
+                if (String.Compare(_keys[i], targetKey) == 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        // FindEmpty -- Find room in the array for a new entry.
+        private int FindEmpty()
+        {
+            for (int i = 0; i < _keys.Length; i++)
+            {
+                if (_keys[i] == null)
+                {
+                    return i;
+                }
+            }
+
+            throw new Exception("Array is full");
+        }
+
+        // Look up contents by string key -- this is the indexer 
+        public object this[string key]
+        {
+            set
+            {
+                // See if the string is already there 
+                int index = Find(key);
+                if (index < 0)
+                {
+                    // It isn't -- find a new spot.
+                    index = FindEmpty();
+                    _keys[index] = key;
+                }
+                // Save the object in the corresponding spot 
+                _arrayElements[index] = value;
+            }
+
+            get
+            {
+                int index = Find(key);
+                if (index < 0)
+                {
+                    return null;
+                }
+
+                return _arrayElements[index];
+            }
         }
     }
 }
