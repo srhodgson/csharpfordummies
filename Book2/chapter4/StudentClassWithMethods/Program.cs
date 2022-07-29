320408366fcb027194703c94e0cae7b0a287cc25
Stephen Hodgson <stephen@srhodgson.co.uk>
2022-07-29 22:47:01 +0100

    Added two simple programs to demostrate passing objects

---
+++ Book2/chapter4/StudentClassWithMethods/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+/**
+ * StudentClassWithMethods
+ * Demonstrate putting methods that operate on a class's data inside the class 
+ * A class is responsible for it's own data and any operation on it.abstract 
+ */
+
+namespace StudentClassWithMethods
+{
+    // Now the OutputName and SetName method sare members of the class Student,
+    // not class Program
+    public class Student
+    {
+        public string name;
+
+        // OutputName -- Output the student's name 
+        public static void OutputName(Student student)
+        {
+            // Output current student's name.
+            Console.WriteLine("Student's name is {0}", student.name);
+        }
+
+        // SetName -- Modify the student object's name 
+        public static void SetName(Student student, string name)
+        {
+            student.name = name;
+        }
+    }
+
+    public class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            // Set the name by accessing it directly 
+            Console.WriteLine("The first time: ");
+            student.name = "Stephen";
+            Student.OutputName(student); // Method now belongs to student
+
+            // Change the name using a method 
+            Console.WriteLine("After being modified: ");
+            Student.SetName(student, "Robert");
+            Student.OutputName(student);
+
+            // Wait for user to acknowledge 
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
